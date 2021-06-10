5625e3ab9d417b3c0df248d98634a51912390f80
maria <marianen@live.com.ar>
2021-06-10 20:10:34 -0300

    Ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,68 @@
+import logging
+import random
+import time
+from regionCondicional import *
+
+
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+class Recurso1(Recurso):
+    dato1 = 0
+    numEscritores = 0
+
+recurso1 = Recurso1()
+
+def condicionLector():
+    return recurso1.numEscritores == 0
+
+region1 = RegionCondicional(recurso1, condicionLector)
+region2 = Region(recurso1)
+
+
+
+@region1.condicion
+def leer():
+    logging.info(f'Lector lee dato1 = {recurso1.dato1}')
+
+
+
+@region2.region
+def escribir():
+    recurso1.numEscritores += 1
+    recurso1.dato1 = random.randint(0, 100)
+    logging.info(f'Escritor escribe dato1 = {recurso1.dato1}')
+    time.sleep(1)
+    recurso1.numEscritores -= 1
+
+
+
+
+def Lector():
+    while True:
+        leer()
+        time.sleep(random.randint(3, 6))
+
+def Escritor():
+    while True:
+        escribir()
+        time.sleep(random.randint(1,4))
+
+
+
+
+
+def main():
+    nlector = 10
+    nescritor = 2
+
+    for k in range(nlector):
+        threading.Thread(target=Lector, daemon=True).start()
+
+    for k in range(nescritor):
+        threading.Thread(target=Escritor, daemon=True).start()
+
+    time.sleep(300)
+
+
+if __name__ == "__main__":
+    main()
